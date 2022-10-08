9f1e1ec6a98451190985164e000edd1f272cbc1c
darksnakepy <darkhuntshowdown@gmail.com>
2022-10-08 11:41:01 +0200

    updated

---
+++ Intellij/contoCorrente/src/classes/CC.java
@@ -0,0 +1,15 @@
+package classes;
+
+import java.util.Scanner;
+
+public class CC {
+    String nome = "";
+    String cognome = "";
+    String CF = "";
+    int pin = 0;
+    float saldo = 0;
+    Scanner inp = new Scanner(System.in);
+    final Float fido = -3000.0F;
+    boolean opened = false;
+}
+
--- Intellij/contoCorrente/src/classes/main.java
+++ Intellij/contoCorrente/src/classes/main.java
@@ -1,8 +1,12 @@
 package classes;
+import classes.CC;
+import classes.startContoCorrente;
 
 public class main {
     public static void Main(String[] args)
     {
-        
+        int choice; 
+        System.out.println("1. Versare soldi\n2. Prelevare soldi sul conto corrente\n3. Visualizza il saldo\n4. Esci");
+
     }
 }
