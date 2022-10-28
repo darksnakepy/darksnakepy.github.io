ce0aaa49d0602fe896a56dea4a176673d29e39c2
darksnakepy <darkhuntshowdown@gmail.com>
2022-10-28 11:57:47 +0200

    updated

---
+++ Intellij/findNumbers/src/packages/Main.java
@@ -0,0 +1,13 @@
+package packages;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        static int[] generaArr(int size){
+            Math.Random();
+
+
+        }
+    }
+}
