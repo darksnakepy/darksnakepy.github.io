c569235d15eb72994490e99446b7ba997ed2eb4d
darksnakepy <darkhuntshowdown@gmail.com>
2023-03-14 12:53:39 +0100

    esercizio inheritance officina

---
+++ Java Projects/Officina/src/Main.java
@@ -0,0 +1,97 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.ArrayList;
+/*
+    Scrivere una classe Officina che prevede solo un metodo ripara() che utilizza veicoli
+    (come definiti nell'esercizio precedente). Tale metodo prende un veicolo come parametro, ne cambia (se
+    necessario) il valore della variabile booleana che descrive lo stato di guasto e restituisce come risultato
+    il prezzo dell'intervento. Il prezzo deve variare a seconda che il veicolo fosse guasto o meno, e a seconda
+    della tipologia di veicolo.
+    Per testare le classi, scrivere un programma UsaOfficina che crea un po' di veicoli e un oggetto di
+    tipo Officina, e usa il metodo ripara() varie volte su oggetti diversi (guasti o meno) stampando i
+    prezzi ottenuti.
+ */
+class Main{
+    public static void main(String[] args){
+        Veicolo veicolo1= new Veicolo("123", "undefined", "Lamborghini", true);
+        Veicolo veicolo2 = new Veicolo("615", "undefined", "Mustang", true);
+        Officina officina = new Officina();
+
+        String response = officina.ripara(veicolo1);
+        System.out.println(response);
+        System.out.println("Il prezzo è di: "+veicolo2.price());
+
+    }
+}
+
+class Officina {
+
+    public static String ripara(Veicolo veicolo){
+        if(veicolo.visualizzaStato()){
+            switch(veicolo.getMarca()) {
+                case "Lamborghini":
+                    veicolo.changePrice(veicolo, 5000);
+                    veicolo.cambiaStatus(veicolo);
+                    break;
+                case "Mustang":
+                    veicolo.changePrice(veicolo, 4000);
+                    veicolo.cambiaStatus(veicolo);
+                    break;
+            }
+            return "riparato";
+        }
+        else{
+            return "Il veicolo non è rotto.";
+        }
+    }
+}
+
+class Veicolo{
+    private String targa;
+    private String modello;
+    private String marca;
+    private float price;
+    private boolean macchinaRotta = false;
+
+    public Veicolo(String targa, String modello, String marca, boolean macchinaRotta){
+        this.targa = targa;
+        this.modello = modello;
+        this.marca = marca;
+        this.price = 2000;
+        this.macchinaRotta = macchinaRotta;
+    }
+
+    public String getTarga(){
+        return targa;
+    }
+
+    public String getModello(){
+        return modello;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public float price(){
+        return price;
+    }
+
+    public boolean visualizzaStato(){
+        return macchinaRotta;
+    }
+
+    public boolean cambiaStatus(Veicolo veicolo){
+        this.macchinaRotta = false;
+        return this.macchinaRotta;
+    }
+    
+    public float changePrice(Veicolo veicolo, int value) { // to fix change price
+        if(!veicolo.macchinaRotta){
+            return veicolo.price;
+        }
+        else{
+            return veicolo.price;
+        }
+    }
+}
