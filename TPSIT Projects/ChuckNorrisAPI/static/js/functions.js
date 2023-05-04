6e657f45f6abe4f53560327b5ed31a5ff7df9f0f
Giovanni Sansonetti <93006390+darksnakepy@users.noreply.github.com>
2023-05-04 09:59:53 +0200

    Dio

--- TPSIT Projects/ChuckNorrisAPI/static/js/functions.js
+++ TPSIT Projects/ChuckNorrisAPI/static/js/functions.js
@@ -1,27 +1,28 @@
 let btn = document.querySelector(".load-joke")
 let copyBtn = document.querySelector("copy")
 
-let data = ""
 let joke = document.querySelector(".joke")
 let joke_container = document.querySelector(".joke-container")
 
 btn?.addEventListener("click", async (e)=>{
     e.preventDefault()
-    let genre = document.querySelector(".category-selector").value
+    let genre = document.querySelector("select").value
     let url = `https://api.chucknorris.io/jokes/random?category=${genre.toLowerCase()}`
 
     await fetch(url)
         .then(data => {            
-            data = data.json()
+            return data.json()
         })
         .then(jokegen =>{
             if(jokegen.value == undefined){
                 joke_container.innerHTML = "A"
+                return
             }
 
-            joke_container.innerHTML = data.value     
+            joke_container.innerHTML = jokegen.value     
         })
         .catch(err => {
+            console.error(err)
             joke_container.innerHTML = "Error"
         })
 })
