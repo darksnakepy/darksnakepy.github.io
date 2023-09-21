b7867833f5b8fee180a7514ff1f98cb7208a124b
darksnakepy <darkhuntshowdown@gmail.com>
2023-09-21 10:53:11 +0200

    map component now shows the marker with coordinates

--- TPSIT Projects/Weather/src/components/MainPage/MainPage.tsx
+++ TPSIT Projects/Weather/src/components/MainPage/MainPage.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState, ChangeEvent, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Map from './Map/Map';
-import { layerGroup } from 'leaflet';
 
 const MainPage = () =>{
 
@@ -11,10 +9,12 @@ const MainPage = () =>{
     const [error, setError] = useState("")
     const inputLatitudeChange = useRef<HTMLInputElement>(null), inputLongitudeChange = useRef<HTMLInputElement>(null)
 
+    const [currentLatitude, setCurrentLatitude] = useState(0)
+    const [currentLongitude, setCurrentLongitude] = useState(0)
 
     useEffect(() =>{
         getCurrentLocation()
-        console.log(parseFloat(latitude), parseFloat(longitude))
+        console.log(currentLatitude, currentLongitude)
     }, [])
 
     const sendData = async () =>{
@@ -40,15 +40,20 @@ const MainPage = () =>{
     }
 
 
-    const getCurrentLocation = async () => {
-        const permission = await navigator.permissions.query({ name: 'geolocation' });
-        navigator.geolocation.getCurrentPosition((position) => {
-        const currentLatitude = position.coords.latitude;
-        const currentLongitude = position.coords.longitude;
-        setLatitude(currentLatitude.toString());
-        setLongitude(currentLongitude.toString());
-        })
-    }
+    const getCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const lati = position.coords.latitude;
+                const longi = position.coords.longitude;
+                console.log(lati, longi);
+                setCurrentLatitude(lati);
+                setCurrentLongitude(longi);
+            },
+            (error) => {
+                console.error("Error getting geolocation:", error);
+            }
+    );
+}
 
     return(
         <div className="w-screen h-screen flex flex-col justify-center items-center">
@@ -56,10 +61,10 @@ const MainPage = () =>{
                 <h1 className="text-[40px] text-white">Weather App</h1>
                 <h2 className="text-white ">Insert the latitude and longitude </h2>
             </div>
-            <Map currentLocation={[parseFloat(latitude), parseFloat(longitude)]}/>
+            <Map currentLocation={[41.1171, 16.8719]} />
             <div className="mt-5 flex flex-row ">
-                <input type="text" placeholder="Latitude" onChange={(e: ChangeEvent<HTMLInputElement>) => setLatitude(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/> 
-                <input type="text" placeholder="Longitude" onChange={(e: ChangeEvent<HTMLInputElement>) => setLongitude(e.target.value)} className="ml-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/> 
+                <input type="text" ref={inputLatitudeChange} placeholder="Latitude" onChange={(e: ChangeEvent<HTMLInputElement>) => setLatitude(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/> 
+                <input type="text" ref={inputLongitudeChange} placeholder="Longitude" onChange={(e: ChangeEvent<HTMLInputElement>) => setLongitude(e.target.value)} className="ml-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/> 
                 <button className="bg-blue-700 hover:bg-blue-800 text-white py-2 px-10 rounded-lg ml-6" onClick={sendData}>
                     Submit
                 </button>
--- TPSIT Projects/Weather/src/components/MainPage/Map/Map.tsx
+++ TPSIT Projects/Weather/src/components/MainPage/Map/Map.tsx
@@ -1,18 +1,30 @@
-import { MapContainer, TileLayer, Marker, useMapEvents  } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, useMapEvents, Popup } from 'react-leaflet'
 import { useState } from 'react';
 import "leaflet/dist/leaflet.css"
+import { marker } from 'leaflet';
 
 interface MapProps {
   currentLocation?: [number, number]
 }
 
+// When the user clicks on the map, the marker will appear at the clicked location with the correct lat & lng values 
+
+const MarkerClick = ({ onMapClick }: { onMapClick: (lat: number, lon: number) => void }) =>{ //
+    const map = useMapEvents({click: (e)=> {
+      const { lat, lng } = e.latlng
+      onMapClick(lat, lng)
+      console.log(lat, lng)
+    }
+    })
+    return null
+}
+
 const Map = ({currentLocation} : MapProps) => {
 
-    const [markers, setMarkers] = useState<[number, number][]>([]);
+  const [markerPosition, setMarkerPosition] = useState<[number, number]>() // we can set the latitude and longitude's values using this hook
 
-    const addMarker = (e: any) => {
-      const newMarker = [e.latlng.lat, e.latlng.lng];
-      
+    const handleMapClick = (lat: number, lon: number) => {  // handle click function to set the marker position
+      setMarkerPosition([lat, lon]);
     };
 
     return(
@@ -21,6 +33,10 @@ const Map = ({currentLocation} : MapProps) => {
           url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.carto.com/attribution">CartoDB</a> contributors'
         />
+        <MarkerClick onMapClick={handleMapClick}/>
+        {markerPosition && <Marker position={markerPosition}> 
+                              <Popup>{markerPosition}</Popup>
+                            </Marker>} {/* displays the marker */}
       </MapContainer>
   )
 }
